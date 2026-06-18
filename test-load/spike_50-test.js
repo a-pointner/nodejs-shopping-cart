a0c55bdcccd4bfcc5e00b8abb0f3d9c3a7d57735
Andreas Pointner <s2430107029@fhooe.at>
2026-06-18 11:49:57 +0200

    implemented k6 spike-tests

---
+++ test-load/spike_50-test.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 10 },
+    { duration: '5s', target: 50 },
+    { duration: '15s', target: 50 },
+    { duration: '5s', target: 0 },
+  ],
+};
+
+export default function () {
+  const res = http.get('http://localhost:3000/');
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+  sleep(1);
+}
